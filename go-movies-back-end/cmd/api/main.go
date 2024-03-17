30033b675986d83490ae16fe5772a72036faadde
Aaron Sullivan <kingaroan@gmail.com>
2024-03-17 20:08:46 +0000

    first commit

---
+++ go-movies-back-end/cmd/api/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"backend/internal/repository"
+	"backend/internal/repository/dbrepo"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+const port = 8080
+
+type application struct {
+	DSN          string
+	Domain       string
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
+}
+
+func main() {
+	// Set application config
+	var app application
+
+	// read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres Connection String")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
+	flag.Parse()
+	// connect to the database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal("cannot connect to database: ", err)
+	}
+
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
+
+	app.auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookieDomain:  app.CookieDomain,
+		CookiePath:    "/",
+		CookieName:    "host-refresh-token",
+	}
+
+	log.Println("Starting server on port", port)
+
+	// start a web server
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
+}
---
+++ go-movies-back-end/cmd/api/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(app.enableCORS)
+
+	mux.Get("/", app.Home)
+
+	mux.Post("/authenticate", app.authenticate)
+
+	mux.Get("/movies", app.AllMovies)
+
+	return mux
+}
